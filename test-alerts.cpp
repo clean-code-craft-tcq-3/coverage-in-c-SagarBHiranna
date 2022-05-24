7c00cde369432f6889e1511ac3bb65320adade31
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-05-24 21:35:33 +0530

    Add files via upload

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,12 +5,10 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(-12, 0, 35) == TOO_LOW);
-  REQUIRE(inferBreach(101,100,105) == NORMAL);
+  REQUIRE(inferBreach(101,100,105) == TOO_HIGH);
   REQUIRE(inferBreach(25,20,30)==NORMAL);
-  REQUIRE(inferBreach(0, 0, 35) == NORMAL);
-  REQUIRE(inferBreach(105,100,105) == NORMAL);
-  REQUIRE(inferBreach(98,100,105) == TOO_LOW);
-  REQUIRE(inferBreach(190,100,105) == TOO_HIGH);
+  REQUIRE(inferBreach(0, 0, 35) == TOO_LOW);
+  REQUIRE(inferBreach(105,100,105) == TOO_HIGH);
 }
 
 TEST_CASE("Check for Upper limit check functionality") {
@@ -26,4 +24,37 @@ TEST_CASE("Check for lower limit check functionality") {
   REQUIRE(checkValueInLowerLimit(-102,-100) == 1);
 }
 
+TEST_CASE("Check for Display Functionality") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 37) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 45) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 20) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 40) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 48) == TOO_HIGH);
+}
 
+TEST_CASE("Check for Mail sending functionality") {
+  CHECK_NOTHROW(sendToEmail(NORMAL, displayOnConsole));
+  CHECK_NOTHROW(sendToEmail(TOO_LOW, displayOnConsole));
+  CHECK_NOTHROW(sendToEmail(TOO_HIGH, displayOnConsole));
+}
+
+TEST_CASE("Check for controller informing functionality") {
+  CHECK_NOTHROW(sendToController(NORMAL, displayOnConsole));
+  CHECK_NOTHROW(sendToController(TOO_LOW, displayOnConsole));
+  CHECK_NOTHROW(sendToController(TOO_HIGH, displayOnConsole));
+}
+
+TEST_CASE("Check for displayOnConsole Functionality") {
+  CHECK_NOTHROW(displayOnConsole("Hello There !!"));
+
+}
