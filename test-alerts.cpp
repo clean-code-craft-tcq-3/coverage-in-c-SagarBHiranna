8f9d5847b85ab1e7f3600efd9b911ff074fb99b0
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-05-25 00:43:46 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -43,26 +43,15 @@ TEST_CASE("Check for Display Functionality") {
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 48) == TOO_HIGH);
 }
 
-TEST_CASE(" Cooling Properties: Check and Alert Verification") {
-  BatteryCharacter batterProperties1 = {PASSIVE_COOLING, "ABCD"};
-  checkAndAlert(TO_CONTROLLER, batterProperties1, 20, displayOnConsole);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5)==TOO_LOW);
-	checkAndAlert(TO_EMAIL, batterProperties1, -5, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5) == TOO_LOW);
-	checkAndAlert(TO_CONTROLLER, batterProperties1, 70, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 70) == TOO_HIGH);
-	BatteryCharacter batterProperties2 = {HI_ACTIVE_COOLING, "ABCD"};
-	checkAndAlert(TO_EMAIL, batterProperties2, 22, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 22) == NORMAL);
-	checkAndAlert(TO_EMAIL, batterProperties2, -6, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -6) == TOO_LOW);
-	checkAndAlert(TO_CONTROLLER, batterProperties2, 80, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 80) == TOO_HIGH);
-  BatteryCharacter batterProperties3 = {MED_ACTIVE_COOLING, "ABCD"};
-	checkAndAlert(TO_CONTROLLER, batterProperties3, 21, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 21) == NORMAL);
-	checkAndAlert(TO_CONTROLLER, batterProperties3, -9, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -9) == TOO_LOW);
-	checkAndAlert(TO_EMAIL, batterProperties3, 90, displayOnConsole);
-	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 90) == TOO_HIGH);
+TEST_CASE("Check the value and alert for Passive Cooling") {
+	BatteryCharacter batteryCharacter;
+	batteryCharacter.coolingType = PASSIVE_COOLING;
+	checkAndAlert(TO_CONTROLLER, batteryCharacter, 15, print);
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 15) == NORMAL);
+
+	checkAndAlert(TO_EMAIL, batteryCharacter, -10, fakePrint);
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -10) == TOO_LOW);
+
+	checkAndAlert(TO_CONTROLLER, batteryCharacter, 40, print);
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
 }
