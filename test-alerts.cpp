01fb3988bdc1fbccaf05e82893e8c76f61cd67d5
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-05-24 18:34:40 +0530

    Add files via upload

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,6 +4,24 @@
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(-12, 0, 35) == TOO_LOW);
+  REQUIRE(inferBreach(101,100,105) == TOO_HIGH);
+  REQUIRE(inferBreach(25,20,30)==NORMAL);
+  REQUIRE(inferBreach(0, 0, 35) == TOO_LOW);
+  REQUIRE(inferBreach(105,100,105) == TOO_HIGH);
 }
+
+TEST_CASE("Check for Upper limit check functionality") {
+  REQUIRE(checkValueInUpperLimit(100, 99) == 1);
+  REQUIRE(checkValueInUpperLimit(100,100) == 0);
+  REQUIRE(checkValueInUpperLimit(-98,100) == 0);
+}
+
+TEST_CASE("Check for lower limit check functionality") {
+  REQUIRE(checkValueInLowerLimit(10, 0) == 0);
+  REQUIRE(checkValueInLowerLimit(10,-100) == 0);
+  REQUIRE(checkValueInLowerLimit(-98,100) == 1);
+  REQUIRE(checkValueInLowerLimit(-102,-100) == 1);
+}
+
 
