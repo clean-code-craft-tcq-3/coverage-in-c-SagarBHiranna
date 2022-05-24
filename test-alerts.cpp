48e13bb3ae0f9a107421dc06dd4a4da9d578c539
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-05-25 01:26:14 +0530

    Add files via upload

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -25,13 +25,19 @@ TEST_CASE("Check for lower limit check functionality") {
   REQUIRE(checkValueInLowerLimit(-102,-100) == 1);
 }
 
+TEST_CASE("Check the functionality of mail notification")
+{
+  REQUIRE(sendToEmail(NORMAL)==E_NOT_OK);
+  REQUIRE(sendToEmail(TOO_HIGH)==E_OK);
+}
+
 TEST_CASE("Cooling Properties : Check and Alert TestCases") {
   BatteryCharacter batteryProp1 = {PASSIVE_COOLING, "ABCD"};
   BatteryCharacter batteryProp2 = {HI_ACTIVE_COOLING, "BCDE"};
   BatteryCharacter batteryProp3 = {MED_ACTIVE_COOLING, "PQRS"};
   checkAndAlert(TO_CONTROLLER, batteryProp1, 20.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20.0)==NORMAL);
-  checkAndAlert(TO_EMAIL, batteryProp1, -5.0);
+  checkAndAlert(TO_CONTROLLER, batteryProp1, -5.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5.0)==TOO_LOW);
   checkAndAlert(TO_CONTROLLER, batteryProp1, 70.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 70.0)==TOO_HIGH);
@@ -39,11 +45,11 @@ TEST_CASE("Cooling Properties : Check and Alert TestCases") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 22.0)==NORMAL);
   checkAndAlert(TO_CONTROLLER, batteryProp2, -6.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -6.0)==TOO_LOW);
-  checkAndAlert(TO_EMAIL, batteryProp2, 80.0);
+  checkAndAlert(TO_CONTROLLER, batteryProp2, 80.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 80.0)==TOO_HIGH);
   checkAndAlert(TO_CONTROLLER, batteryProp3, 20.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20.0)==NORMAL);
-  checkAndAlert(TO_EMAIL, batteryProp3, -9.0);
+  checkAndAlert(TO_CONTROLLER, batteryProp3, -9.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -9.0)==TOO_LOW);
   checkAndAlert(TO_CONTROLLER, batteryProp3, 90.0);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 90)==TOO_HIGH);
