65c05ff8b8a4d2e85dc8a68c17ded661161a9e2b
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-05-25 00:40:44 +0530

    Add files via upload

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -44,27 +44,25 @@ TEST_CASE("Check for Display Functionality") {
 }
 
 TEST_CASE(" Cooling Properties: Check and Alert Verification") {
-  BatteryCharacter batterProperties ;
-  batterProperties.coolingType = PASSIVE_COOLING;
-  checkAndAlert(TO_CONTROLLER, batterProperties, 20, displayOnConsole);
+  BatteryCharacter batterProperties1 = {PASSIVE_COOLING, "ABCD"};
+  checkAndAlert(TO_CONTROLLER, batterProperties1, 20, displayOnConsole);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5)==TOO_LOW);
-	checkAndAlert(TO_EMAIL, batterProperties, -5, displayOnConsole);
+	checkAndAlert(TO_EMAIL, batterProperties1, -5, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5) == TOO_LOW);
-	checkAndAlert(TO_CONTROLLER, batterProperties, 70, displayOnConsole);
+	checkAndAlert(TO_CONTROLLER, batterProperties1, 70, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 70) == TOO_HIGH);
-	batterProperties.coolingType = HI_ACTIVE_COOLING;
-	checkAndAlert(TO_EMAIL, batterProperties, 22, displayOnConsole);
+	BatteryCharacter batterProperties2 = {HI_ACTIVE_COOLING, "ABCD"};
+	checkAndAlert(TO_EMAIL, batterProperties2, 22, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 22) == NORMAL);
-	checkAndAlert(TO_EMAIL, batterProperties, -6, displayOnConsole);
+	checkAndAlert(TO_EMAIL, batterProperties2, -6, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -6) == TOO_LOW);
-	checkAndAlert(TO_CONTROLLER, batterProperties, 80, displayOnConsole);
+	checkAndAlert(TO_CONTROLLER, batterProperties2, 80, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 80) == TOO_HIGH);
-  batterProperties.coolingType = MED_ACTIVE_COOLING;
-	checkAndAlert(TO_CONTROLLER, batterProperties, 21, displayOnConsole);
+  BatteryCharacter batterProperties3 = {MED_ACTIVE_COOLING, "ABCD"};
+	checkAndAlert(TO_CONTROLLER, batterProperties3, 21, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 21) == NORMAL);
-	checkAndAlert(TO_CONTROLLER, batterProperties, -9, displayOnConsole);
+	checkAndAlert(TO_CONTROLLER, batterProperties3, -9, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -9) == TOO_LOW);
-	checkAndAlert(TO_EMAIL, batterProperties, 90, displayOnConsole);
+	checkAndAlert(TO_EMAIL, batterProperties3, 90, displayOnConsole);
 	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 90) == TOO_HIGH);
 }
-
