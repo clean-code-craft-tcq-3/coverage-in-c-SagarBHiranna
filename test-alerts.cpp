26831a6f3191b0f556ad4583c7b91013becc41df
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-05-24 22:25:09 +0530

    Add files via upload

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -7,14 +7,14 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(-12, 0, 35) == TOO_LOW);
   REQUIRE(inferBreach(101,100,105) == NORMAL);
   REQUIRE(inferBreach(25,20,30)==NORMAL);
-  REQUIRE(inferBreach(0, 0, 35) == NORMAL);
-  REQUIRE(inferBreach(105,100,105) == NORMAL);
+  REQUIRE(inferBreach(0, 0, 35) == TOO_LOW);
+  REQUIRE(inferBreach(105,100,105) == TOO_HIGH);
   REQUIRE(inferBreach(120,100,105) == TOO_HIGH);
 }
 
 TEST_CASE("Check for Upper limit check functionality") {
   REQUIRE(checkValueInUpperLimit(100, 99) == 1);
-  REQUIRE(checkValueInUpperLimit(100,100) == 0);
+  REQUIRE(checkValueInUpperLimit(100,100) == 1);
   REQUIRE(checkValueInUpperLimit(-98,100) == 0);
 }
 
@@ -26,25 +26,24 @@ TEST_CASE("Check for lower limit check functionality") {
 }
 
 TEST_CASE("Check for Display Functionality") {
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 0) == TOO_LOW);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 35) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 37) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 0) == TOO_LOW);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 45) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 45) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 20) == NORMAL);
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 0) == TOO_LOW);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 40) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 40) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20) == NORMAL);
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 48) == TOO_HIGH);
 }
 
 TEST_CASE("Check for Mail sending functionality") {
-  statusLog statusInfo ;
   REQUIRE(sendToEmail(NORMAL, displayOnConsole)==E_NOT_OK);
   REQUIRE(sendToEmail(TOO_LOW, displayOnConsole)==E_OK);
   REQUIRE(sendToEmail(TOO_HIGH, displayOnConsole)==E_OK);
